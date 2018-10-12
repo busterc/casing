67ce348b150e158886a541aeffbda494705fbae6
Bruno Leonardo Michels <brunolm7@gmail.com>
2018-10-11 22:17:20 -0300

    Add TypeScript definition file

---
+++ main.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Deeply converts object keys to camelCase
+ */
+export declare const camelize: (obj: any) => any;
+
+/**
+ * Deeply converts object keys to snake_case
+ */
+export declare const snakeize: (obj: any) => any;
